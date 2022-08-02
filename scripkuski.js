22891c001251a3e55d50275291dcd07550c87c0b
Vasya <Vasilii@Air-Vasilii.sknt.ru>
2022-08-02 16:30:27 +0300

    first commit

---
+++ scripkuski.js
@@ -0,0 +1,25 @@
+"use strict";
+    let number = 7;
+    const person = "Alex";
+    const bool = true;
+
+
+    const obj = {
+        name : "John",
+        age : 25,
+        married : false
+
+    };
+    //console.log(obj.name);
+    console.log(obj["name"]);
+
+
+    let arr = ["xui.png" , "pizda.jpg" , "djigurda.png", 6 , {} , []];
+    console.log(arr[3]);
+
+    let storeName="jhsdfgsdhg";
+    let storeDescription = { 
+        budget : 10000,
+        employees : arr = ["kek", "lox", "pidr"],
+};
+console.log(storeDescription.employees[2]);
